7af29a0b5d12caa99b3d45c3341ed1c4e7cbd0c5
jhs512 <jangka512@gmail.com>
2021-02-19 13:49:46 +0900

    TitleBar 컴포넌트 전역화

--- src/pages/ArticleListPage.vue
+++ src/pages/ArticleListPage.vue
@@ -4,13 +4,9 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import TitleBar from '../components/TitleBar.vue';
 
 export default defineComponent({
   name: 'ArticleListPage',
-  components: {
-    TitleBar
-  }
 })
 </script>
 
