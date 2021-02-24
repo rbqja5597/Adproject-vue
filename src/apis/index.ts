f797aee164d1f3dcbc1c21b3c41221099ca63ba2
jhs512 <jangka512@gmail.com>
2021-02-24 20:01:36 +0900

    글 작성

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -74,6 +74,12 @@ export interface MainApi__article_detail__IResponseBody extends Base__IResponseB
   };
 }
 
+export interface MainApi__article_doWrite__IResponseBody extends Base__IResponseBodyType1 {
+  body:{
+    id: number
+  };
+}
+
 // http://localhost:8021/usr/ 와의 통신장치
 export class MainApi extends HttpClient {
   public constructor() {
@@ -87,6 +93,7 @@ export class MainApi extends HttpClient {
   protected _handleRequest(config:AxiosRequestConfig) {
     config.params = {};
     config.params.authKey = localStorage.getItem("authKey");
+    
     return config;
   };
 
@@ -107,5 +114,9 @@ export class MainApi extends HttpClient {
   // http://localhost:8021/usr/detail/id?id=? 를 요청하고 응답을 받아오는 함수
   public article_detail(id: number) {
     return this.instance.get<MainApi__article_detail__IResponseBody>(`/article/detail?id=${id}`);
+  }
+
+  public article_doWrite(boardId:number, title: string, body: string) {
+    return this.instance.get<MainApi__article_doWrite__IResponseBody>(`/article/doAdd?boardId=${boardId}&title=${title}&body=${body}`);
   }
 }
--- src/pages/ArticleDetailPage.vue
+++ src/pages/ArticleDetailPage.vue
@@ -1,41 +1,13 @@
 <template>
-  <TitleBar>게시물 리스트</TitleBar>
+  <TitleBar>게시물 상세</TitleBar>
   
-  <section class="section section-article-list px-2">
+  <section class="section section-article-write-form px-2">
     <div class="container mx-auto">
-      <div class="mt-6" v-bind:key="article.id" v-for="article in state.articles">
-        <div class="px-10 py-6 bg-white rounded-lg shadow-md">
-          <div class="flex justify-between items-center">
-            <span class="font-light text-gray-600">
-              {{ article.regDate }}
-            </span>
-            <a href="#" class="px-2 py-1 bg-gray-600 text-gray-100 font-bold rounded hover:bg-gray-500">
-              {{ article.boardId == 1 ? "공지" : "자유" }}
-            </a>
-          </div>
-          <div class="mt-2">
-            <a href="#" class="text-2xl text-gray-700 font-bold hover:underline">
-              {{ article.title }}
-            </a>
-            <p class="mt-2 text-gray-600">
-              {{ article.body }}
-            </p>
-          </div>
-          <div class="flex justify-between items-center mt-4">
-            <router-link :to="'detail?id=' + article.id" class="text-blue-500 hover:underline">
-              자세히 보기
-            </router-link>
-            <div>
-              <router-link :to="'detail?id=' + article.id" class="flex items-center">
-                <img src="https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=731&amp;q=80" alt="avatar" class="mx-4 w-10 h-10 object-cover rounded-full hidden sm:block">
-                <h1 class="text-gray-700 font-bold hover:underline">{{article.extra__writer}}</h1>
-              </router-link>
-            </div>
-          </div>
-        </div>
-      </div>
+      
     </div>
   </section>
+
+  
 </template>
 
 <script lang="ts">
