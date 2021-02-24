7cc4faf8f2d63ec4d9c06ab8fbaa123279f710aa
jhs512 <jangka512@gmail.com>
2021-02-25 04:17:23 +0900

    글 작성시 게시판 선택가능

--- src/pages/ArticleWritePage.vue
+++ src/pages/ArticleWritePage.vue
@@ -4,6 +4,12 @@
   <section class="section section-article-write-form px-2">
     <div class="container mx-auto">
       <form v-on:submit.prevent="checkAndWriteArticle">
+        <FormRow title="게시판">
+          <select class="form-row-select" ref="newArticleBoardIdElRef">
+            <option value="1">공지사항</option>
+            <option value="2">자유</option>
+          </select>
+        </FormRow>
         <FormRow title="제목">
           <input ref="newArticleTitleElRef" class="form-row-input" type="text" placeholder="제목을 입력해주세요.">
         </FormRow>
@@ -21,7 +27,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, reactive, getCurrentInstance, onMounted, watch } from 'vue'
+import { defineComponent, ref, reactive, getCurrentInstance, onMounted } from 'vue'
 import { IArticle } from '../types/'
 import { MainApi } from '../apis/'
 import { Router } from 'vue-router';
@@ -39,14 +45,29 @@ export default defineComponent({
     const router:Router = getCurrentInstance()?.appContext.config.globalProperties.$router;
     const mainApi:MainApi = getCurrentInstance()?.appContext.config.globalProperties.$mainApi;
 
+    const newArticleBoardIdElRef = ref<HTMLInputElement>();
     const newArticleTitleElRef = ref<HTMLInputElement>();
     const newArticleBodyElRef = ref<HTMLInputElement>();
+
+    onMounted(() => {
+      if ( newArticleBoardIdElRef.value == null ) {
+        return;
+      }
+      
+      newArticleBoardIdElRef.value.value = props.boardId + "";
+    })
 
     const state = reactive({
       articles: [] as IArticle[]
     });
 
     function checkAndWriteArticle() {
+      if ( newArticleBoardIdElRef.value == null ) {
+        return;
+      }
+
+      const newArticleBoardIdEl = newArticleBoardIdElRef.value;
+
       if ( newArticleTitleElRef.value == null ) {
         return;
       }
@@ -77,7 +98,7 @@ export default defineComponent({
         return;
       }
 
-      writeArtile(newArticleTitleEl.value, newArticleBodyEl.value);
+      writeArtile(parseInt(newArticleBoardIdEl.value), newArticleTitleEl.value, newArticleBodyEl.value);
 
       newArticleTitleEl.value = '';
       newArticleBodyEl.value = '';
@@ -85,8 +106,8 @@ export default defineComponent({
       newArticleTitleEl.focus();
     }
 
-    function writeArtile(title:string, body:string) {
-      mainApi.article_doWrite(props.boardId, title, body)
+    function writeArtile(boardId:number, title:string, body:string) {
+      mainApi.article_doWrite(boardId, title, body)
         .then(axiosResponse => {
           const newArticleId = axiosResponse.data.body.id;
           alert(newArticleId + "번 글이 생성되었습니다.");
@@ -98,6 +119,7 @@ export default defineComponent({
     return {
       state,
       checkAndWriteArticle,
+      newArticleBoardIdElRef,
       newArticleTitleElRef,
       newArticleBodyElRef
     }
